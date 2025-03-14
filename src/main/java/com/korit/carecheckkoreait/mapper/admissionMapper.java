96c9432f80b5bef3ea232a7b6d10183ca2327bd0
freemuras <freemuras@gmail.com>
2025-03-14 17:45:11 +0900

    mapper.xml 작업중 v2

--- src/main/java/com/korit/carecheckkoreait/mapper/admissionMapper.java
+++ src/main/java/com/korit/carecheckkoreait/mapper/admissionMapper.java
@@ -1,5 +1,7 @@
 package com.korit.carecheckkoreait.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.korit.carecheckkoreait.entity.Admission;
@@ -7,4 +9,7 @@ import com.korit.carecheckkoreait.entity.Admission;
 @Mapper
 public interface admissionMapper {  
     int insertAdmission (Admission admission);
+    List<Admission> selectWaitingList (String usercode);
+    List<Admission> selectVitalInfo (int admId);
+    List<Admission> selectDetailOrderByAdmId(int admId);
 } 
