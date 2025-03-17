f3feb38df60253e2120b5d750ca503929b3263af
june1704 <xvz1704@naver.com>
2025-03-17 16:04:26 +0900

    수정

---
+++ src/main/java/com/korit/carecheckkoreait/controller/ChartController.java
@@ -0,0 +1,26 @@
+package com.korit.carecheckkoreait.controller;
+
+import com.korit.carecheckkoreait.dto.request.ReqAddChartDto;
+import com.korit.carecheckkoreait.dto.request.ReqSearchChartDto;
+import com.korit.carecheckkoreait.service.ChartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ChartController {
+
+    @Autowired
+    private ChartService chartService;
+
+    @PostMapping("/chart/registration")
+    public ResponseEntity<?> registerChart(@RequestBody ReqAddChartDto reqAddChartDto) {
+        return ResponseEntity.ok(chartService.addChart(reqAddChartDto));
+    }
+
+    @GetMapping("/chart")
+    public ResponseEntity<?> searchPatient(@ModelAttribute ReqSearchChartDto reqSearchChartDto) throws Exception {
+        return ResponseEntity.ok().body(chartService.getPatientId(reqSearchChartDto));
+    }
+
+}
