519eeaefaf2e3820b9c78967d8dddd730ec7b8ac
yunggeun0627 <bbiyagi465@gmail.com>
2025-04-01 17:20:21 +0900

    PatientsPage 관련 메서드 추가

--- src/main/java/com/korit/carecheckkoreait/repository/PatientRepository.java
+++ src/main/java/com/korit/carecheckkoreait/repository/PatientRepository.java
@@ -5,6 +5,7 @@ import com.korit.carecheckkoreait.mapper.PatientMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,5 +20,17 @@ public class PatientRepository {
 
     public Optional<Patient> getPatientId(int patientId) {
         return Optional.ofNullable(patientMapper.selectPatientById(patientId));
+    }
+
+    public int selectPatientsCount(String patientName) {
+        return patientMapper.selectPatientsCount(patientName);
+    }
+
+    public Optional<List<Patient>> selectPatientsByName(
+            int startIndex,
+            int limitCount,
+            String patientName
+    ) {
+        return Optional.ofNullable(patientMapper.selectPatientsByName(startIndex, limitCount, patientName));
     }
 }
