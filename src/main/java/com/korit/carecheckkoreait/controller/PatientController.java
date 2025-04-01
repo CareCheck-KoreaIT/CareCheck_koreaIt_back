8cded3372622751a6f88a9cfeba18f53b9d090b9
tkdgns2232 <tkdgns2232@naver.com>
2025-04-01 15:51:06 +0900

    수정

--- src/main/java/com/korit/carecheckkoreait/controller/PatientController.java
+++ src/main/java/com/korit/carecheckkoreait/controller/PatientController.java
@@ -5,21 +5,22 @@ import com.korit.carecheckkoreait.entity.Patient;
 import com.korit.carecheckkoreait.service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/patients")
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
 
     @Operation(summary = "환자등록추가", description = "환자등록")
-    @PostMapping("/patients")
+    @PostMapping("")
     public ResponseEntity<?> registerPatient(@RequestBody ReqPatientRegDto reqPatientRegDto) {
         Patient patient = Patient.builder()
                 .patientName(reqPatientRegDto.getPatientName())
@@ -30,6 +31,19 @@ public class PatientController {
                 .build();
 
         return ResponseEntity.ok().body(patientService.addPatient(patient));
+    }
+
+    @Operation(summary = "환자 번호 조회", description = "환자 번호 조회")
+    @GetMapping("/{patientId}")
+    public ResponseEntity<?> getPatientById(@PathVariable("patientId") int patientId) {
+        Optional<Patient> patient = patientService.getPatientId(patientId);
+
+        if (patient.isPresent()) {
+            return ResponseEntity.ok().body(patient.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("해당 환자 ID가 존재하지 않습니다.");
+        }
     }
 }
 
--- src/main/java/com/korit/carecheckkoreait/service/PatientService.java
+++ src/main/java/com/korit/carecheckkoreait/service/PatientService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class PatientService {
     @Autowired
@@ -14,5 +16,10 @@ public class PatientService {
     @Transactional(rollbackFor = Exception.class)
     public Patient addPatient(Patient patient) {
         return patientRepository.save(patient);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public Optional<Patient> getPatientId(int patientId) {
+        return patientRepository.getPatientId(patientId);
     }
 }
